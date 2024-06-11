1f4111b27325be916243f61c6663d1947372be57
Rafael <raas@cin.ufpe.br>
2024-06-10 23:33:44 -0300

    Comentários e Readme organizados
    Co-authored-by: João Fellows <JoaoFellows@users.noreply.github.com>
    Co-authored-by: hlpaa <hlpaa@users.noreply.github.com>

--- Cliente/client.py
+++ Cliente/client.py
@@ -2,9 +2,6 @@ import socket as skt
 import time
 import os
 
-#binding => ip:porta => localhost:7070
-# MAX_BUFF => tam max msg (bytes)
-
 MAX_BUFF_SIZE = 1024 # Bytes (1KB)
 
 addr_bind = ('localhost', 8080) # porta que o cliente será vinculado
@@ -15,12 +12,12 @@ class UDPClient():
         self.sckt = skt.socket(sckt_family, sckt_type)
         self.sckt.bind(sckt_binding)
         self.sckt.settimeout(0.1)
-        self.EOF_MARKER = b"EOF"
+        self.EOF_MARKER = b"EOF" #Variável criada para marcar o final do arquivo
 
         if self.sckt is None:
             raise "Socket not available."
     
-        self.MAX_BUFF = MAX_BUFF
+        self.MAX_BUFF = MAX_BUFF #Tamanho máximo do buffer;
 
     def send(self, server_addr: tuple[str, str], msg: bytes):
         # client_addr: (localhost, 8080)
@@ -30,33 +27,31 @@ class UDPClient():
     def send_file(self, file_path, server_addr: tuple[str, str]):
         with open(file_path, 'rb') as file:
             while True:
-                data = file.read(self.MAX_BUFF)
+                data = file.read(self.MAX_BUFF) #Lê o arquivo em partes iguais ao MAX_BUFF, e continua de onde parou até o final do arquivo.
                 if not data:
                     print("File sent.")
                     break
-                self.send(server_addr, data)
-        self.send(server_addr, self.EOF_MARKER)
+                self.send(server_addr, data)    #Envia os dados do arquivo.
+        self.send(server_addr, self.EOF_MARKER) #Envia para o servidor um sinal de que o arquivo acabou.
         
     def listen(self):
         print("Listening (client)...")
         while True:
             try:
-                nome, end = self.sckt.recvfrom(self.MAX_BUFF)
-                #os.rename(nome.decode(), 'Funcionou_'+nome.decode())
+                nome, end = self.sckt.recvfrom(self.MAX_BUFF) #Recebe o nome do arquivo do servidor, já alterado.
                 if nome: 
                     break
             except:
                 continue
-        with open(nome.decode(), 'wb') as file: 
+        with open(nome.decode(), 'wb') as file:           #Recebe a devolução do arquivo pelo servidor.
             while True:
                 try: 
                     data, addr = self.sckt.recvfrom(self.MAX_BUFF)
                     if data == self.EOF_MARKER:
-                        print("EOF marker received. File transfer complete.")
+                        print("EOF marker received. File transfer complete.") 
                         break
                     if data:
                         file.write(data)
-                        #print(f"{data.decode()}")
                         print(f"Received data from {addr}")
                     else:
                         break
@@ -72,8 +67,8 @@ class UDPClient():
 def main():
     client = UDPClient(skt.AF_INET, skt.SOCK_DGRAM, addr_bind, MAX_BUFF_SIZE)
     print("Client started.")
-    client.send(addr_target, 'Imagem.png'.encode())
-    client.send_file('Imagem.png', addr_target)
-    client.listen()
+    client.send(addr_target, 'Imagem.png'.encode())      #Envia o nome do arquivo para o servidor.
+    client.send_file('Imagem.png', addr_target)          #Envia o arquivo para o servidor.
+    client.listen()                                      #Recebe a devolução do arquivo pelo servidor.
 
 main()
--- Servidor/server.py
+++ Servidor/server.py
@@ -2,50 +2,39 @@ import socket as skt
 import time
 import os
 
-#binding => ip:porta => localhost:7070
-# MAX_BUFF => tam max msg (bytes)
-
+#Criando a classe do servidor
 class UDPServer():
     def __init__(self, sckt_family, sckt_type, sckt_binding, MAX_BUFF):
         self.sckt = skt.socket(sckt_family, sckt_type)
         self.sckt.bind(sckt_binding)
         self.sckt.settimeout(5.0)
-
+        #Verifica se o socket foi criado.
         if self.sckt is None:
             raise "Socket not available."
-    
+        
+        #Define o tamanho máximo do buffer.
         self.MAX_BUFF = MAX_BUFF
+        #Variável criada para marcar o final do arquivo enviado.
         self.EOF_MARKER = b"EOF"
 
-    def listen2(self, client_addr: tuple[str, str]):
-        print("Listening (server)...")
-        while True:
-            try:
-                data, origin = self.sckt.recvfrom(self.MAX_BUFF)
-            except:
-                continue
-
     def send(self, client_addr: tuple[str, str], msg: bytes):
-        # client_addr: (localhost, 8080)
         self.sckt.sendto(msg, client_addr)
         time.sleep(0.0001)
 
     def send_file(self, file_path, client_addr: tuple[str, str]):
-        #new_file_path = 'Modified_' + file_path
-        #os.rename(file_path, new_file_path)
         with open(file_path, 'rb') as file:
             while True:
                 data = file.read(self.MAX_BUFF)
                 if not data:
                     break
                 self.send(client_addr, data)
-        self.send(client_addr, self.EOF_MARKER)
+        self.send(client_addr, self.EOF_MARKER) #envia o marcador, indicando o final do arquivo.
 
     def listen(self):
         print("Listening (server)...")
         while True:
             try:
-                nome, end = self.sckt.recvfrom(self.MAX_BUFF)
+                nome, end = self.sckt.recvfrom(self.MAX_BUFF) #Recebe o nome do arquivo enviado pelo cliente.
                 if nome: 
                     break
             except:
@@ -56,12 +45,10 @@ class UDPServer():
                     data, addr = self.sckt.recvfrom(self.MAX_BUFF)
                     if data == self.EOF_MARKER:
                         print("EOF marker received. File transfer complete.")
-                        #os.rename(nome.decode(), 'Funcionou_'+nome.decode())
-                        self.send(addr, ('Modified_'+nome.decode()).encode())
+                        self.send(addr, ('Modified_'+nome.decode()).encode()) #envia o nome do arquivo que foi recebido com o nome alterado.
                         break
                     if data:
                         file.write(data)
-                        #print(f"{data.decode()}")
                         print(f"Received data from {addr}")
                     else:
                         break
@@ -70,17 +57,18 @@ class UDPServer():
                 except Exception as e:
                     print(f"An error occurred: {e}")
                     break
-        self.send_file('Server_'+nome.decode(), addr)
+        self.send_file('Server_'+nome.decode(), addr) #envia o arquivo que foi recebido de volta para o cliente.
 
 MAX_BUFF_SIZE = 1024 # Bytes (1KB)
 
-addr_bind = ('127.0.0.1', 7070) # porta que o servidor será vinculado
-addr_target = ('localhost', 8080) # porta que o servidor irá enviar dados (cliente)
+addr_bind = ('127.0.0.1', 7070) # porta que o servidor será vinculado.
+addr_target = ('localhost', 8080) # porta que o servidor irá enviar dados (cliente).
 
 
 def main():
+
     server = UDPServer(skt.AF_INET, skt.SOCK_DGRAM, addr_bind, MAX_BUFF_SIZE)
     print("Server started.")
-    server.listen()
+    server.listen()     #O servidor começa a escutar.
 
 main()
